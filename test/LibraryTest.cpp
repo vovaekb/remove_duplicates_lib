48f7686a40e5528a13890a9e9ee46282fbfed6e2
vlad <vladprivalov1990@gmail.com>
2024-07-12 00:35:56 +0400

    Small refactoring

--- test/LibraryTest.cpp
+++ test/LibraryTest.cpp
@@ -2,10 +2,10 @@
 #include <iostream>
 
 #include <gtest/gtest.h>
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
+// #include <opencv2/core.hpp>
+// #include <opencv2/imgcodecs.hpp>
+// #include <opencv2/imgproc.hpp>
+// #include <opencv2/highgui.hpp>
 
 #include "DuplicatesDeleter.h"
 
